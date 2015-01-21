caf23f4e36db65863c79ac54452d40d1f5e2b1e5
Dominique Feyer <dfeyer@ttree.ch>
2015-01-21 18:44:14 +0100

    [TASK] Initial commit

---
+++ Classes/Ttree/Yubico/Authentication/Token/UsernamePassword.php
@@ -0,0 +1,74 @@
+<?php
+namespace Ttree\Yubico\Authentication\Token;
+
+/*                                                                        *
+ * This script belongs to the Ttree.Yubico package.                       *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Mvc\ActionRequest;
+use TYPO3\Flow\Reflection\ObjectAccess;
+use TYPO3\Flow\Security\Authentication\Token\AbstractToken;
+
+/**
+ * An authentication token used for simple username and password authentication.
+ */
+class UsernamePassword extends AbstractToken {
+
+	/**
+	 * The username/password credentials
+	 * @var array
+	 * @Flow\Transient
+	 */
+	protected $credentials = array('username' => '', 'password' => '', 'token' => '');
+
+	/**
+	 * Updates the username, password credentials and the yubikey token from the POST vars, if the POST parameters
+	 * are available. Sets the authentication status to REAUTHENTICATION_NEEDED, if credentials have been sent.
+	 *
+	 * Note: You need to send the username and password in these two POST parameters:
+	 *       __authentication[Ttree][Yubico][Authentication][Token][UsernamePassword][username]
+	 *   and __authentication[Ttree][Yubico][Authentication][Token][UsernamePassword][password]
+	 *   and __authentication[Ttree][Yubico][Authentication][Token][UsernamePassword][token]
+	 *
+	 * @param ActionRequest $actionRequest The current action request
+	 * @return void
+	 */
+	public function updateCredentials(ActionRequest $actionRequest) {
+		$httpRequest = $actionRequest->getHttpRequest();
+		if ($httpRequest->getMethod() !== 'POST') {
+			return;
+		}
+
+		$arguments = $actionRequest->getInternalArgument('__authentication');
+		if (!is_array($arguments)) {
+			return;
+		}
+		$username = ObjectAccess::getPropertyPath($arguments, 'Ttree.Yubico.Authentication.Token.UsernamePassword.username');
+		$password = ObjectAccess::getPropertyPath($arguments, 'Ttree.Yubico.Authentication.Token.UsernamePassword.password');
+		$token = ObjectAccess::getPropertyPath($arguments, 'Ttree.Yubico.Authentication.Token.UsernamePassword.token');
+
+		if (!empty($username) && !empty($password)) {
+			$this->credentials['username'] = $username;
+			$this->credentials['password'] = $password;
+			$this->credentials['token'] = $token;
+			$this->setAuthenticationStatus(self::AUTHENTICATION_NEEDED);
+		}
+	}
+
+	/**
+	 * Returns a string representation of the token for logging purposes.
+	 *
+	 * @return string The username credential
+	 */
+	public function  __toString() {
+		return vsprintf('Username: "%s" with OTP (%s)', array($this->credentials['username'], $this->credentials['token']));
+	}
+
+}
---
+++ Classes/Ttree/Yubico/Service/OneTimePasswordService.php
@@ -0,0 +1,48 @@
+<?php
+namespace Ttree\Yubico\Service;
+
+/*                                                                        *
+ * This script belongs to the Ttree.Yubico package.                       *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use Doctrine\ORM\Mapping as ORM;
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Log\SystemLoggerInterface;
+use Yubikey\Validate;
+
+/**
+ * One Time Password (OTP) Service
+ *
+ * @Flow\Scope("singleton")
+ */
+class OneTimePasswordService {
+
+	/**
+	 * @Flow\InjectConfiguration("api")
+	 * @var array
+	 */
+	protected $apiSettings;
+
+	/**
+	 * @Flow\Inject
+	 * @var SystemLoggerInterface
+	 */
+	protected $systemLogger;
+
+	/**
+	 * @param string $token
+	 * @return boolean
+	 */
+	public function check($token) {
+		$validate = new Validate($this->apiSettings['secrectKey'], $this->apiSettings['clientId']);
+		$responses = $validate->check($token);
+		return $responses->success();
+	}
+
+}
