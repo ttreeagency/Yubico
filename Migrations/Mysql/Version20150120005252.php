6b906928c3bca042ac7d8ed5416121fea3cff4f5
Dominique Feyer <dfeyer@ttree.ch>
2017-04-08 11:42:59 +0200

    TASK: Fix configuration and migration

--- Migrations/Mysql/Version20150120005252.php
+++ Migrations/Mysql/Version20150120005252.php
@@ -15,9 +15,9 @@ class Version20150120005252 extends AbstractMigration {
 	 */
 	public function up(Schema $schema) {
 		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
-		
-		$this->addSql("CREATE TABLE ttree_yubico_domain_model_key (publicid VARCHAR(12) NOT NULL, account VARCHAR(40) DEFAULT NULL, INDEX IDX_25D26A9A7D3656A4 (account), PRIMARY KEY(publicid))");
-		$this->addSql("ALTER TABLE ttree_yubico_domain_model_key ADD CONSTRAINT FK_25D26A9A7D3656A4 FOREIGN KEY (account) REFERENCES typo3_flow_security_account (persistence_object_identifier)");
+
+        $this->addSql('CREATE TABLE ttree_yubico_domain_model_key (publicid VARCHAR(12) NOT NULL, account VARCHAR(40) DEFAULT NULL, INDEX IDX_671E50F77D3656A4 (account), PRIMARY KEY(publicid)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE ttree_yubico_domain_model_key ADD CONSTRAINT FK_671E50F77D3656A5 FOREIGN KEY (account) REFERENCES neos_flow_security_account (persistence_object_identifier)');
 	}
 
 	/**
@@ -26,7 +26,7 @@ class Version20150120005252 extends AbstractMigration {
 	 */
 	public function down(Schema $schema) {
 		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
-		
+
 		$this->addSql("DROP TABLE ttree_yubico_domain_model_key");
 	}
 }
